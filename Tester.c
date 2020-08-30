700e80351f7fe8188cd1913c3d3e58095f2244ce
BalaSathishKumar <70202635+BalaSathishKumar@users.noreply.github.com>
2020-08-30 10:20:49 +0530

    Create Tester.c

---
+++ Tester.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+printf(“size of char =%d\n”, sizeof(char));
+printf(“size of short=%d\n”, sizeof(short));
+printf(“size of int =%d\n”, sizeof(int));
+printf(“size of unsigned int=%d\n”, sizeof(unsigned));
+printf(“size of long int=%d\n”, sizeof(long));
+printf(“size of unsigned long int=%d\n”, sizeof(unsigned
+long));
+printf(“size of float =%d\n”, sizeof(float));
+printf(“size of double=%d\n”, sizeof(double));
+printf(“size of long double%d\n”, sizeof(long double));
+}
